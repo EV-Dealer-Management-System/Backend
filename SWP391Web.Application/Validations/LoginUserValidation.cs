4afac2afeba7c4db379953e05516f11060069a27
hoangtuzami <hoangtuzami@gmail.com>
2025-08-28 21:15:10 +0700

    Create functions which forgot password, send reset password mail and reset with userId, token and password. Add Dockerfile to cicd

--- SWP391Web.Application/Validations/LoginUserValidation.cs
+++ SWP391Web.Application/Validations/LoginUserValidation.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SWP391Web.Application.Validation
+namespace SWP391Web.Application.Validations
 {
     public class LoginUserValidation : AbstractValidator<LoginUserDTO>
     {
