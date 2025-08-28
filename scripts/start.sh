f5f0566d3b611ce1463c586b43ed72d9aae086b9
Trần Đức Hiệu <157787787+hoangtuzami@users.noreply.github.com>
2025-08-29 02:42:50 +0700

    Delete scripts directory

--- scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ACCOUNT_ID=886350084162
-REGION=ap-southeast-1
-IMAGE_NAME=simple-docker-service
-CONTAINER_NAME=simple-docker-service
-
-echo "Starting new container"
-docker run -d --name $CONTAINER_NAME -p 80:80 $ACCOUNT_ID.dkr.ecr.$REGION.amazonaws.com/$IMAGE_NAME:latest
--- scripts/validate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Validating service"
-curl -f http://localhost:80 || exit 1
