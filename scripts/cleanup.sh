3a7173753a65a85839fc4b82f083f480353585dc
Trần Đức Hiệu <157787787+hoangtuzami@users.noreply.github.com>
2025-08-29 00:13:52 +0700

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 CONTAINER_NAME=simple-docker-service
 
-if [ $(docker ps -a -q -f name=$CONTAINER_NAME) ]; then
+chmod +x scripts/*.sh
+
+CONTAINER_ID=$(docker ps -a -q -f name=$CONTAINER_NAME)
+if [ -n "$CONTAINER_ID" ]; then
   echo "Removing old container"
-  docker rm -f $CONTAINER_NAME
+  docker rm -f $CONTAINER_ID
 fi
