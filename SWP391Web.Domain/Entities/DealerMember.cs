a94ca76d1a53682477900fe7d735a91978075cb5
hoangtuzami <hoangtuzami@gmail.com>
2025-09-23 14:34:43 +0700

    Add databases that dealer and dealer member

---
+++ SWP391Web.Domain/Entities/DealerMember.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391Web.Domain.Entities
+{
+    public class DealerMember
+    {
+        public Guid DealerMemberId { get; set; }
+        public Guid DealerId { get; set; }
+        public string UserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        private readonly List<ApplicationUser> _users = new();
+        public IReadOnlyCollection<ApplicationUser> Users => _users.AsReadOnly();
+        public Dealer Dealer { get; set; } = null!;
+    }
+}
