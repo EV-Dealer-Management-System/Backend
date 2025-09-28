8a2c77e66280bc5e3b2b9d50aebb166b1323423e
hoangtuzami <hoangtuzami@gmail.com>
2025-09-28 11:19:48 +0700

    Create a function which get all province from GHN

---
+++ SWP391Web.Domain/ValueObjects/GhnWard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391Web.Domain.ValueObjects
+{
+    public class GhnWard
+    {
+        public string? WardCode { get; set; }
+        public int DistrictID { get; set; }
+        public string WardName { get; set; } = string.Empty;
+        public List<string>? NameExtension { get; set; }
+    }
+}
---
+++ SWP391Web.Infrastructure/IRepository/IGHNClient.cs
@@ -0,0 +1,16 @@
+﻿using SWP391Web.Domain.ValueObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391Web.Infrastructure.IRepository
+{
+    public interface IGHNClient
+    {
+        Task<GhnResult<List<GhnProvince>>> GetProvincesAsync(CancellationToken ct = default);
+        Task<GhnResult<List<GhnDistrict>>> GetDistrictsAsync(CancellationToken ct = default);
+        Task<GhnResult<List<GhnWard>>> GetWardsAsync(int districtId, CancellationToken ct = default);
+    }
+}
