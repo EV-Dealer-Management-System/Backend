3af8684f5e14f2fbc490ddc6b93d62c345d749f5
minhduy <MDuy10x203@gmail.com>
2025-10-06 17:17:38 +0700

    Create 2 entities booking and bookingDetails

---
+++ SWP391Web.Domain/Entities/BookingElectricVehicle.cs
@@ -0,0 +1,20 @@
+﻿using SWP391Web.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391Web.Domain.Entities
+{
+    public class BookingElectricVehicle
+    {
+        public Guid Id { get; set; }
+        public Guid DealerId { get; set; } // Đại lý thực hiện booking
+        public DateTime BookingDate { get; set; };
+        public BookingStatus Status { get; set; } // Enum trạng thái
+        public int TotalQuantity { get; set; }
+        public string? Note { get; set; }
+        public string? CreatedBy { get; set; }
+    }
+}
---
+++ SWP391Web.Domain/Entities/BookingElectricVehicleDetail.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391Web.Domain.Entities
+{
+    public class BookingElectricVehicleDetail
+    {
+        public Guid Id { get; set; }
+        public Guid BookingId { get; set; }
+        public Guid VersionId { get; set; }
+        public Guid ColorId { get; set; }
+        public int Quantity { get; set; }
+        public DateTime? ExpectedDeliveryDate { get; set; }
+        
+    }
+}
