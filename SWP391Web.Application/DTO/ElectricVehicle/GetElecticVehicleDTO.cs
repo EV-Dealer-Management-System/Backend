2ac44aa1213d2abb984e9f632bab54c933d84982
minhduy <MDuy10x203@gmail.com>
2025-10-17 15:09:43 +0700

    Update entities

--- SWP391Web.Application/DTO/ElectricVehicle/GetElecticVehicleDTO.cs
+++ SWP391Web.Application/DTO/ElectricVehicle/GetElecticVehicleDTO.cs
@@ -21,7 +21,7 @@ namespace SWP391Web.Application.DTO.ElectricVehicle
         public DateTime? ImportDate { get; set; }
         public DateTime? WarrantyExpiryDate { get; set; }
         public decimal CostPrice { get; set; }
-        public string? ImageUrl { get; set; }
+        
     }
     public class ViewVersionName
     {
--- SWP391Web.Domain/Entities/Quote.cs
+++ SWP391Web.Domain/Entities/Quote.cs
@@ -12,6 +12,7 @@ namespace SWP391Web.Domain.Entities
         public Guid Id { get; set; }
         public Guid WarehouseId { get; set; } 
         public Guid DealerId { get; set; } 
+        public Guid PromotionId { get; set; }
         public string CreatedById { get; set; } = null!; 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public QuoteStatus Status { get; set; }
