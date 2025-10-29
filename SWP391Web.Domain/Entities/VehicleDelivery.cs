5aa7fc6a42a558d5b2890c104c7cdcbdf5e2b41a
minhduy <MDuy10x203@gmail.com>
2025-10-29 15:21:51 +0700

    Add Entiti VehicleDelivery

---
+++ SWP391Web.Domain/Entities/VehicleDelivery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWP391Web.Domain.Entities
+{
+    public class VehicleDelivery
+    {
+        public Guid Id { get; set; }
+        public Guid BookingEVId { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+        public DeliveryStatus Status { get; set; } // enum: Preparing, Packing, InTransit, Arrived, Confirmed, Accident
+        public DateTime? UpdateAt { get; set; } = DateTime.UtcNow;
+    }
+}
