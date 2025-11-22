852bf4766b94b421a80ff3da22a9c9c8c6d8f0d4
hoangtuzami <hoangtuzami@gmail.com>
2025-11-22 19:27:18 +0700

    Fix error that customer cannot confirm remaining econtract

--- SWP391Web.Application/Services/CustomerOrderService.cs
+++ SWP391Web.Application/Services/CustomerOrderService.cs
@@ -663,6 +663,7 @@ namespace SWP391Web.Application.Services
                 else if (isCash is null)
                 {
                     await _eContractService.CreatePayFullConfirmationEContract(customerOrder.Id, ct);
+                    customerOrder.Status = OrderStatus.RemainingPending;
                     _unitOfWork.CustomerOrderRepository.Update(customerOrder);
                     await _unitOfWork.SaveAsync(ct);
                     return new ResponseDTO
